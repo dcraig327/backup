d853c4c67bb1e7349b26fb70d5abdcf1d094ae29
David Craig <dcraig327@gmail.com>
2021-02-16 03:09:27 -0800

    add license to project

--- home/home.bak.sh
+++ home/home.bak.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
-
+##########################################################################
+# backup - backup & restore your data
+# Copyright (C) 2021 David Craig
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##########################################################################
+# home.bak.sh
+#
 # A script to perform incremental backups using rsync
+##########################################################################
 
 #set -o errexit
 set -o nounset

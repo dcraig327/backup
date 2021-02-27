75f330af1cf98636488cd6e675c8dddf478fa7f0
David Craig <dcraig327@gmail.com>
2021-02-26 23:13:48 -0800

    update backup

--- js/backup.js
+++ js/backup.js
@@ -19,10 +19,42 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 const execSync = require("child_process").execSync;
 const env = require("process").env;
+require('dotenv').config();
+
+//SOURCE_DIR        Directory of what to backup
+//BACKUP_DIR        Directory of Where to backup
+//LATEST_LINK       File that points to latest backup
+//EXCLUDE_LIST      File that contains exclude list
+//LOG_FILE          File for logs
+
+  // /%([^%]+)%/g, (_, n) => process.env[n])
+
+
+//this evaluates '~/downloads' and should check for ~
+//fooDir = fooDir.replace(/^~\//, process.env.HOME + '/');
+
+//This function doesn't handle the special variables: $0,$!,$$,$?,$-,$#,$@
+//No clue what's going on , heading to learn RegEx
+function evaluate(command) {
+  // const original = command;
+  // console.log(command);
+  // command = command.replace(/(${)([^{]+)}/g, (_,n) => { console.log(n); return process.env[n] || _; });
+  // console.log(command);
+  // command = command.replace(/([^$]+)/g, (_, n) => { console.log(n); return process.env[n] || _; });
+  // console.log(command);
+  // while (original !== command) {
+  //   command = evaluate(command);
+  // }
+  // console.log(command);
+  return command;
+}
 
 function sh(command) {
   let exitCode = 0;
   let outString;
+
+  command = evaluate(command);
+
   try {
     outString = execSync(command);
   } catch (err) {
@@ -32,9 +64,7 @@ function sh(command) {
     outString = outString.toString();
   }
   if (outString) {
-    //this removes all line breaks at the end of string for linux,mac,windows
-    //need to only remove the very last linebreak
-    outString = outString.replace(/(\r\n|\n|\r)$/gm, "");
+    outString = outString.replace(/(\r\n|\n|\r).*$/, "");
     console.log(outString);
   }
   return exitCode;
@@ -45,22 +75,38 @@ function pgrep(process) {
   return sh(`pgrep -n ${process} > /dev/null`);
 }
 
-//env.BIN_DIR="${HOME}/bin/backup/root";
-env.BIN_DIR="${HOME}/bin";
-env.SOURCE_DIR="/";
-env.BACKUP_DIR="/pool/backups/root";
-env.DATETIME="$(date '+%Y-%m-%d_%H:%M:%S')";
-env.BACKUP_PATH="${BACKUP_DIR}/${DATETIME}";
-env.LATEST_LINK="${BACKUP_DIR}/latest";
-env.EXCLUDE_LIST="${BIN_DIR}/exclude.root.list";
-env.LOG_FILE="${BIN_DIR}/backup.root.log";
+// while (!pgrep("rsync")) {
+//   sh("sleep 1");
+// }
 
-while (!pgrep("rsync")) {
-  sh("sleep 1");
-}
+//sh("echo ~");
+//sh("echo $PATH");
+//sh("echo ${PATH}");
 
-sh("echo $HOME");
-sh("echo $BIN_DIR");
+sh("echo ${LOG_DIR}");
+
+
+// sh("pwd");
+// sh("echo $HOME");
+
+// sh("echo $SOURCE_DIR");
+// sh("echo $BACKUP_DIR");
+// sh("echo $BIN_DIR");
+// sh("echo $DATETIME");
+
+// sh(`echo ${env.SOURCE_DIR}`);
+// sh(`echo ${env.DATETIME}`);
+// sh(`echo ${env.BIN_DIR}`);
+// sh(`echo ${env.LATEST_LINK}`);
+
+// //$(date '+%Y-%m-%d_%H:%M:%S')
+// sh("date '+%Y-%m-%d_%H:%M:%S'");
+
+// env.DT = "date '+%Y-%m-%d_%H:%M:%S'";
+// sh(`${env.DT}`);
+// //sh("echo $DT");
+// //sh("date '+%Y-%m-%d_%H:%M:%S'");
+
 
 // sh(
 //   'echo rsync -aAXv --dry-run --delete "${SOURCE_DIR}/" --exclude-from="${EXCLUDE_LIST}" "${BACKUP_PATH}" > "${LOG_FILE}" 2>&1'
@@ -72,11 +118,14 @@ sh("echo $BIN_DIR");
 //rsync -aAXv --delete "${SOURCE_DIR}/" --link-dest "${LATEST_LINK}" --exclude-from="${EXCLUDE_LIST}" "${BACKUP_PATH}" > "${LOG_FILE}" 2>&1
 
 // sh(
-//   'rsync -aAXv --dry-run --delete "${SOURCE_DIR}/" --exclude-from="${EXCLUDE_LIST}" "${BACKUP_PATH}" > "${LOG_FILE}" 2>&1'
+//   `echo rsync -aAXv --dry-run --delete "${SOURCE_DIR}/" --exclude-from="${EXCLUDE_LIST}" "${BACKUP_PATH}" > "${LOG_FILE}" 2>&1`
 // );
-// sh('echo `date` > "${LOG_FILE}"');
+//sh("echo \`date\` >" + `${env.LOG_FILE}`);
+//sh(`echo ${env.LOG_FILE}`);
 
 //sh('rm -rf "${LATEST_LINK}"');
 //sh('ln -s "${BACKUP_PATH}" "${LATEST_LINK}"');
 
 //kill env vars and instead do everything in js
+
+// sh("echo `${TEST}`");

9fc9ce534bb5783e32ab2d3c8048ca0523037aac
David Craig <37514553+dcraig327@users.noreply.github.com>
2021-03-02 02:32:47 -0800

    update

--- js/backup.js
+++ js/backup.js
@@ -21,6 +21,150 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 const execSync = require("child_process").execSync;
 const env = require("process").env;
 
+function print_variables() {
+  output_message(`${env.BACKUP_TYPE}`);
+  output_message(`${env.SOURCE_DIR}`);
+  output_message(`${env.BACKUP_DIR}`);
+  output_message(`${env.BACKUP_PATH}`);
+  output_message(`${env.BACKUP_SUFFIX}`);
+  output_message(`${env.LATEST_LINK}`);
+  output_message(`${env.EXCLUDE_LIST}`);
+  output_message(`${env.LOG_FILE}`);
+}
+
+function output_message(str) {
+  if (env.LOG_FILE) {
+    sh(`echo ${str} >> ${env.LOG_FILE}`);
+  } else {
+    console.log(str);
+  }
+}
+
+function validate_variables() {
+  let return_value = true;
+  let output_string = "";
+
+  const source_dir_desc = "SOURCE_DIR\t\t\t\tSource irectory to backup\n";
+  const backup_dir_desc =
+    "BACKUP_DIR\t\t\t\tDestination directory to store backup\n";
+  const backup_suffix_desc = "BACKUP_SUFFIX\t\t\tappended to backup_edir\n";
+  const latest_link_desc =
+    "LATEST_LINK\t\t\t\tSymlink to latest backup directory\n";
+  const exclude_list_desc =
+    "EXCLUDE_LIST\t\t\tFile that contains exclude list\n";
+  const log_file_desc = "(*) LOG_FILE\t\t\t\t\tFile for logs\n";
+  const backup_type_desc = 'BACKUP_TYPE either "inc" OR "full"';
+
+  // TODO: If no directories exist in backup folder,
+  //or latest link doesn't point to a directory, then we just exit
+  //but we should create a new non-incremental backup and create that latest link
+  if (!is_link_valid()) {
+    output_string += latest_link_desc;
+    return_value = false;
+  }
+
+  if (!is_backup_type_valid()) {
+    output_string += backup_type_desc;
+    return_value = false;
+  }
+
+  if (!env.SOURCE_DIR) {
+    output_string += source_dir_desc;
+    return_value = false;
+  }
+
+  if (!env.BACKUP_DIR) {
+    if (env.BACKUP_TYPE === "inc") {
+      output_string += "(~) ";
+    } else {
+      return_value = false;
+    }
+
+    output_string += backup_dir_desc;
+  }
+
+  if (!env.EXCLUDE_LIST) {
+    output_string += exclude_list_desc;
+    return_value = false;
+  }
+
+  if (!env.LOG_FILE) {
+    output_string += log_file_desc;
+  }
+
+  if (!env.BACKUP_SUFFIX) {
+    output_string += backup_suffix_desc;
+  }
+
+  //if any output string, this will prevent string from ending with "\n" stopping a trim() call
+  if (output_string) {
+    output_string +=
+      "(*) - optional / (!) - not used for BACKUP_TYPE / (~) - optional for BACKUP_TYPE";
+    output_string = "" + output_string;
+  }
+
+  output_message(output_string);
+}
+
+function is_backup_type_valid() {
+  if (!env.BACKUP_TYPE) {
+    return false;
+  } else if (env.BACKUP_TYPE !== "inc" && env.BACKUP_TYPE !== "full") {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function is_link_valid() {
+  //check symlink to directory exists
+  let latest_exists = sh(
+    `if [[ -L ${env.LATEST_LINK} && -d ${env.LATEST_LINK} ]]; then echo yes; else echo no; fi`,
+    true
+  );
+
+  if ("yes" === latest_exists) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function setup_variables() {
+  if (env.BACKUP_DIR) {
+    env.BACKUP_PATH = env.BACKUP_DIR;
+    if (env.BACKUP_SUFFIX) {
+      env.BACKUP_PATH += "/";
+      env.BACKUP_PATH += sh(`${env.BACKUP_SUFFIX}`, true);
+    }
+  }
+}
+
+function create_backup_path() {
+  if (env.BACKUP_PATH) {
+    sh(`mkdir -p ${env.BACKUP_PATH}`);
+  }
+}
+
+function is_root() {
+  if (env.USER === "root") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//returns 0 if process active
+function pgrep(process) {
+  return sh(`pgrep -n ${process} > /dev/null`);
+}
+
+function wait_for_no_rsync() {
+  while (!pgrep("rsync")) {
+    sh("sleep 1");
+  }
+}
+
 function evaluate(command) {
   let original = command;
 
@@ -76,102 +220,23 @@ function sh(command, quiet = false) {
   return exitCode;
 }
 
-//returns 0 if process active
-function pgrep(process) {
-  return sh(`pgrep -n ${process} > /dev/null`);
-}
-
-function setup_variables() {
-  env.BACKUP_PATH = env.BACKUP_DIR;
-  if (env.BACKUP_SUFFIX) {
-    env.BACKUP_PATH += "/";
-    env.BACKUP_PATH += sh(`${env.BACKUP_SUFFIX}`, true);
-  }
-}
-
-function print_variables() {
-  sh("echo ${SOURCE_DIR}");
-  sh("echo ${BACKUP_DIR}");
-  sh("echo ${BACKUP_PATH}");
-  sh("echo ${BACKUP_SUFFIX}");
-  sh("echo ${LATEST_LINK}");
-  sh("echo ${EXCLUDE_LIST}");
-  sh(`echo ${env.LOG_FILE}`);
-  sh("echo ----");
-  console.log(env);
-}
-
-function is_root() {
-  if (env.USER === "root") {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function wait_for_no_rsync() {
-  while (!pgrep("rsync")) {
-    sh("sleep 1");
-  }
-}
-
-function missing_variables() {
-  let output_string = "";
-
-  const source_dir_desc = "SOURCE_DIR\t\t\t\tSource irectory to backup\n";
-  const backup_dir_desc =
-    "BACKUP_DIR\t\t\t\tDestination directory to store backup\n";
-  const backup_suffix_desc = "BACKUP_SUFFIX\t\t\tappended to backup_edir\n";
-  const latest_link_desc =
-    "LATEST_LINK\t\t\t\tFile that points to latest backup\n";
-  const exclude_list_desc =
-    "EXCLUDE_LIST\t\t\tFile that contains exclude list\n";
-  const log_file_desc = "LOG_FILE\t\t\t\t\tFile for logs\n";
-
-  if (!env.SOURCE_DIR) {
-    output_string += source_dir_desc;
-  }
-
-  if (!env.BACKUP_DIR) {
-    output_string += backup_dir_desc;
-  }
-
-  if (!env.LATEST_LINK) {
-    output_string += latest_link_desc;
-  }
-
-  if (!env.EXCLUDE_LIST) {
-    output_string += exclude_list_desc;
-  }
-
-  if (!env.LOG_FILE) {
-    output_string += log_file_desc;
-  }
-
-  //kill ending \n if populated
-  return output_string.trim();
-}
-
-function get_backup_subdirectories() {
-  sh(`find -L ${env.BACKUP_DIR} -maxdepth 1 -type d | sed '1d'`);
-}
-
 function backup() {
-  //if no directories in backup folder, run new backup
-  //env.BACKUP_PATH += sh(`${env.BACKUP_SUFFIX}`, true);
+  if (env.BACKUP_TYPE === "inc") {
+    sh(
+      "rsync -aAXv --delete ${SOURCE_DIR}/ --exclude-from=${EXCLUDE_LIST} ${LATEST_LINK} > ${LOG_FILE} 2>&1"
+    );
+  } else if (env.BACKUP_TYPE === "full") {
+    sh(
+      "rsync -aAXv --delete ${SOURCE_DIR}/ --link-dest ${LATEST_LINK} --exclude-from=${EXCLUDE_LIST} ${BACKUP_PATH} > ${LOG_FILE} 2>&1"
+    );
 
-  //("${BACKUP_DIR}");
-  //if no latest link, then find latest directory and create the latest link
-
-  //run incremental backup
-  sh(
-    "rsync -aAXv --delete ${SOURCE_DIR}/ --link-dest ${LATEST_LINK} --exclude-from=${EXCLUDE_LIST} ${BACKUP_PATH} > ${LOG_FILE} 2>&1"
-  );
+    sh("rm -rf ${LATEST_LINK}");
+    sh("ln -s ${BACKUP_PATH} ${LATEST_LINK}");
+  } else {
+    return 1;
+  }
 
   sh("echo `date` >> ${LOG_FILE}");
-
-  sh("rm -rf ${LATEST_LINK}");
-  sh("ln -s ${BACKUP_PATH} ${LATEST_LINK}");
 }
 
 function main() {
@@ -185,20 +250,15 @@ function main() {
     return 1;
   }
 
-  let invalid_variables = missing_variables();
-  if (invalid_variables) {
-    const invalid_variables_error = "Missing variables: \n" + invalid_variables;
-    if (env.LOG_FILE) {
-      sh(`echo ${invalid_variables_error} >> ${env.LOG_FILE}`);
-    } else {
-      console.log(invalid_variables_error);
-    }
-    return 2;
-  }
+  //TODO: still debugging validate variables for other users
+  //TODO: verify can write/edit all the files/folders pointed to
+  // if (!validate_variables()) {
+  //   return 2;
+  // }
 
   setup_variables();
+  create_backup_path();
   wait_for_no_rsync();
-  print_variables();
   backup();
 }
 
